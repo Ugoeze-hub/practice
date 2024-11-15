0a2bb8b6751b51c14ccdc32abd1e094b5da57f32
Ugoeze <eluchieugoeze@gmail.com>
2024-11-15 15:51:47 +0100

    forage 1

--- forage.py
+++ forage.py
@@ -1,3 +1,4 @@
 import pandas as pd
 cleaned_reaction_types = pd.read_csv("ReactionTypes.csv")
-cleaned_reaction_types.to_csv('cleaned_reaction_types.csv', index = False)
+cleaned_reaction_types.index = range(1, (len(cleaned_reaction_types) + 1))
+cleaned_reaction_types.to_csv('cleaned_reaction_types.csv')
