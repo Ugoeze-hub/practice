d35d75c2aac8ab04aafa37810e61884220f984cd
Ugoeze <eluchieugoeze@gmail.com>
2024-11-19 15:15:33 +0100

    ann

--- clean-data.py
+++ clean-data.py
@@ -1,3 +1,7 @@
 import pandas as pd
 
 BAreviews = pd.read_csv('data_BA_reviews.csv')
+reviews = BAreviews['reviews']
+reviews.str.replace('Trip Verified', '')
+reviews.str.replace('✅', '')
+reviews.str.replace('Verified Review', '')
